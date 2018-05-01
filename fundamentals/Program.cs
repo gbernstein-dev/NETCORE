db3e7cf120c1199ed26cb905bff7f28eb1a2fcb3
GB-actualize <gbactualize@gmail.com>
2018-05-01 04:28:30 -0500

    aspnet practice

---
+++ fundamentals/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+          for (int i = 1; i <= 255; i++)
+            {
+              Console.WriteLine(i);
+            }
+          for (int i = 1; i <= 100; i++)
+            {
+              if (i % 3 == 0 || i % 5 == 0){
+                if (i % 3 == 0 && i % 5 == 0) {}
+                else{
+                  Console.WriteLine(i);
+                }
+            }
+          }
+        }
+    }
+}
