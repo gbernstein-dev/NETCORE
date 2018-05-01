4f306b296001637bc2320dc5c0c72d8a925bfbdb
GB-actualize <gbactualize@gmail.com>
2018-05-01 07:34:15 -0500

    boxing/unboxing

---
+++ boxing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing
+{
+    class Program
+    {
+        static void typing()
+        {
+            int total = 0;
+            int a = 7;
+            int b = 28;
+            int c = -1; 
+            List<object> empty = new List<object>();
+            empty.Add(a);
+            empty.Add(b);
+            empty.Add(c);
+            empty.Add(true);
+            empty.Add("chair");
+            foreach (object item in empty)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(total); 
+        }
+        static void Main(string[] args)
+        {
+            typing();
+        }
+    }
+}
