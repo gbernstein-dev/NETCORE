0e1f8a9ad4e8e8deb4a301db8a94b590f3016dfd
GB-actualize <gbactualize@gmail.com>
2018-05-01 17:48:03 -0500

    human class

---
+++ human/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace human
+{
+    public class HumanBeing
+    {
+      public string name;
+      public HumanBeing(string val)
+      {
+          name = val;
+      }
+      public int health = 100;
+      public int strength = 3;
+      public int intel = 3;
+      public int dexterity = 3;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HumanBeing human1 = new HumanBeing("Jake");
+            Console.WriteLine(human1.name); 
+        }
+    }
+}
